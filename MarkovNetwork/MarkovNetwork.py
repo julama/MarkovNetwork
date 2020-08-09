8efdb6d5196f22ce783393c01e068d27d1077362
julian Amacker <julian.amacker@protonmail.ch>
2020-08-09 22:18:46 +0200

    intital commit decomposable gates

--- MarkovNetwork/MarkovNetwork.py
+++ MarkovNetwork/MarkovNetwork.py
@@ -22,10 +22,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import numpy as np
 
-
-# from numba import jit
-
-
 class MarkovNetwork(object):
     """A Markov Network for neural computing."""
 
